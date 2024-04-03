de442cc8d796d7455d6496f40b53c23e66474d5b
ARPIT <arpitsharma2128@gmail.com>
2024-04-03 05:39:18 +0530

    temp firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,15 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmtER6QL_wsH7vsHW_R-hOquHzVaznlT8",
-  authDomain: "web-ml-blog.firebaseapp.com",
-  projectId: "web-ml-blog",
-  storageBucket: "web-ml-blog.appspot.com",
-  messagingSenderId: "141234077460",
-  appId: "1:141234077460:web:d8c6615373e6b8efd04c01",
-  measurementId: "G-ZJ0E8FFY39",
+  //firebase configuration
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
